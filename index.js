796f22f32bd210cafd5811e2294aca0f016b8da5
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-11-19 14:53:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React, { Component, PropTypes } from 'react';
 
 class RandomTest extends Component {
   static PropTypes = {
-    h1: PropTypes.string,
-    h2: PropTypes.string,
+    h1: PropTypes.string;
+    h2: PropTypes.string;
   }
   render (){
     let {
